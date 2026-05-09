65fbb7f3218e0c1d41a76c1a628d0ad64d6b2af8
beatriz-camilo <abeatrizcamilo@gmail.com>
2026-05-09 13:44:29 -0300

    refactor: generalização melhorada para a adição de novas classes #17

--- src/main/java/br/ufrn/imd/exception/ContaBancariaNaoEncontradaException.java
+++
@@ -1,8 +0,0 @@
-package br.ufrn.imd.exception;
-
-public class ContaBancariaNaoEncontradaException extends RuntimeException {
-
-    public ContaBancariaNaoEncontradaException() {
-        super("Nenhuma conta com esse número foi encontrada.");
-    }
-}
