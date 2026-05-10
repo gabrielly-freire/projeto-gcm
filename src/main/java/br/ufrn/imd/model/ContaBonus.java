19188639a4afb461fb492e38ef4079a9cdce9c21
Gabrielly Freire <gabylopesfreire@gmail.com>
2026-05-10 10:35:58 -0300

    refactor: unifica repository, adiciona regras as operações no dominio e unifica as regras de negocio no service #16 #17

--- src/main/java/br/ufrn/imd/model/ContaBonus.java
+++ src/main/java/br/ufrn/imd/model/ContaBonus.java
@@ -14,6 +14,21 @@ public class ContaBonus extends ContaBancaria {
     }
 
     public void adicionarPontos(int pontos) {
+        if (pontos <= 0) {
+            return;
+        }
         this.pontuacao += pontos;
+    }
+
+    @Override
+    public void creditar(double valor) {
+        super.creditar(valor);
+        adicionarPontos((int) (valor / 100));
+    }
+
+    @Override
+    public void transferirPara(ContaBancaria destino, double valor) {
+        super.transferirPara(destino, valor);
+        adicionarPontos((int) (valor / 200));
     }
 }
