3f39137f62427b6a365395f0be0deb37ce808022
beatriz-camilo <abeatrizcamilo@gmail.com>
2026-06-20 17:06:59 -0300

    merge: aplicação de teste #63

--- src/main/java/br/ufrn/imd/exception/NumeroContaIndisponivel.java
+++
@@ -1,8 +0,0 @@
-package br.ufrn.imd.exception;
-
-public class NumeroContaIndisponivel extends RuntimeException{
-
-    public NumeroContaIndisponivel(String message) {
-        super("Número da conta indisponível: " + message + ".");
-    }
-}
